094ac6172789dc1d7ed806ac543e7851f5fbbb97
Nikita Thomas <nikcxz@gmail.com>
2019-03-19 13:26:31 -0400

    items to rooms + players

--- src/player.py
+++ src/player.py
@@ -1,5 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
+import room
+import item
+
 class player:
-    room
+    currentRoom: room
+
+    items: [item] = []
+
+    def __init__(self, room):
+        self.currentRoom = room
--- src/room.py
+++ src/room.py
@@ -1,10 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+import item
+
 class room:
     name: str
     description: str
 
+    items: [item] = []
+
     def __init__(self, name, desc):
         self.name = name
         self.description = desc
+
+
+    def addItem(self, item):
+        self.items.append(item)
